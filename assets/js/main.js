a64bc172804b53923a7c702900f5333115368093
Ade Adekoya <49321398+adekoyadapo@users.noreply.github.com>
2022-04-27 09:34:40 -0600

    Feature/html plant (#3)
    * [WIP]: New website look - New template
    
    * WIP: phase II
    
    * WIP: Home Page completed
    
    * [WIP]: New website look - Updated template
    
    * [WIP]: New website look - Added contact and details section
    
    * [WIP]: New website look

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,111 +1,166 @@
-//Navbar/Sidebar
-const menuBtn = document.querySelectorAll('.menu-btn');
-const menu = document.querySelector('.menu');
-const links = document.querySelectorAll('.menu li');
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-//Toggle sidebar
-menuBtn.forEach(btn => {
-    btn.addEventListener('click', sideNavToggle);
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+        // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+        // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== whatwedo ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.whatwedo__item')
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.whatwedo__header')
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if (openItem && openItem !== item) {
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.whatwedo__content')
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 400) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+
+/*=============== HOME SWIPER ===============*/
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 });
 
-function sideNavToggle() {
-    let delay = 100;
-    menu.classList.toggle('menu-open');
-    setTimeout(() => {
-        resetAnimations();
-    }, delay * (links.length + 1));
-    links.forEach(link => {
-        link.style.opacity = "0";
-        link.style.animation = "slideIn 400ms ease-in-out forwards";
-        link.style.animationDelay = delay + "ms";
-        delay += 100;
-    });
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: 'true',
+    spaceBetween: 16,
+});
 
-function resetAnimations() {
-    links.forEach(link => {
-        link.style.animation = "none";
-        link.style.opacity = "1";
-    });
-  }
-}
-
-//Slider
-const cntrl = document.querySelectorAll('.slider-cntrl');
-const cntrlMob = document.querySelectorAll('.pagination-mobile > li');
-const title = document.querySelector('.title');
-const subTitle = document.querySelectorAll('.sub-title');
-const img = document.querySelector('.thumbnail');
-const count = document.querySelector('.slider-count');
-const progress = document.querySelector('.progress div');
-
-
-let  id = 0;
-
-const images = [
-    './assets/images/img1.jpeg',
-    './assets/images/img2.jpeg',
-    './assets/images/img3.jpeg',
-];
-
-const progressWidth = [
-    '33%',
-    '66%',
-    '100%',
-];
-
-const text = [
-    'Work',
-    'Active',
-    'Travel',
-];
-
-for(let i = 0; i < cntrl.length; i++) {
-    cntrl[i].addEventListener('click', () => {
-        slider(i);
-        id = i;
-        stopAutoSlide();
-    });
-
-    cntrlMob[i].addEventListener('click', () => {
-        slider(i);
-        id = i;
-        stopAutoSlide();
-    });
-}
-
-function slider(i) {
-    img.src = images[i];
-
-    progress.style.width = progressWidth[i];
-    title.innerText = text[i] + " Collection";
-
-    subTitle.forEach(sub => {
-        sub.innerText = text[i] + " Collection"
-    });
-
-    count.innerText = "/0" + (i + 1);
-
-    for(let i = 0; i < cntrl.length; i++) {
-        cntrl[i].classList.remove('active');
-        cntrlMob[i].classList.remove('pag-active');
-    }
-    
-    cntrl[i].classList.add('active');
-    cntrlMob[i].classList.add('pag-active');
-}
-
-function nexSlide() {
-    id++;
-
-    if (id > cntrl.length - 1) {
-        id = 0;
-    }
-    slider(id);
-}
-
-let autoSlide = setInterval(nexSlide, 10000);
-
-function stopAutoSlide() {
-    clearInterval(autoSlide);
-    autoSlide = setInterval(nexSlide, 10000);
-}
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
+sr.reveal(`.about__img, .tech, .contact__box`, { origin: 'left' })
+sr.reveal(`.about__data, .contact__form`, { origin: 'right' })
+sr.reveal(`.steps__card, .tech, .whatwedo__card, .whatwedo__group, .footer`, { interval: 100 })
